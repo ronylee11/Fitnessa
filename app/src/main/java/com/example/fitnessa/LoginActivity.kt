aa43cff283d55ce9d044c03ce4085cd39c82db03
cleff <rongyil33@gmail.com>
2021-11-12 20:16:18 +0800

    final product

--- app/src/main/java/com/example/fitnessa/LoginActivity.kt
+++ app/src/main/java/com/example/fitnessa/LoginActivity.kt
@@ -1,28 +1,34 @@
 package com.example.fitnessa
 
+import android.annotation.SuppressLint
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.view.View
 import android.view.Window
 import android.view.WindowManager
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 import kotlinx.android.synthetic.main.activity_login.*
-import kotlinx.android.synthetic.main.activity_main.*
 import kotlin.math.roundToInt
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var dataRef: DatabaseReference
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         requestWindowFeature(Window.FEATURE_NO_TITLE)
-        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        this.window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
         setContentView(R.layout.activity_login)
+
+        dataRef = FirebaseDatabase.getInstance().reference
+
         Toast.makeText(this, "All data is open! Please do not use personal password!!", Toast.LENGTH_LONG).show()
         Handler().postDelayed({
             Toast.makeText(this, "Thanks for using this app, have a nice day! ^-^", Toast.LENGTH_LONG).show()
         }, 2000)
+        @SuppressLint("SetTextI18n")
         fun allButtons(id: View){
             when(id){
                 backBtn -> {
@@ -30,15 +36,20 @@ class LoginActivity : AppCompatActivity() {
                 }
                 loginBtn2 -> {
                     val serialId = (Math.random() * 1000000).roundToInt().toString()
-                    val dataRef = FirebaseDatabase.getInstance().reference
+//                    val dataRef = FirebaseDatabase.getInstance().reference
                     val loginID = loginId.text.toString()
                     val loginPWD = loginPassword.text.toString()
                     dataRef.child(serialId).child("Login Credentials").child("ID").setValue(loginID)
                     dataRef.child(serialId).child("Login Credentials").child("Password").setValue(loginPWD)
-                    loginComment.text = "You've logged in!"
+                    loginComment.text = ("You've logged in!")
+                    val intent = Intent(this, MainActivity::class.java)
+                    intent.putExtra("serialId", serialId)
+                    intent.putExtra("loginID", loginID)
                     Handler().postDelayed({
-                        finish()
-                    }, 500)
+                        startActivity(intent)
+                    }, 2000)
+//                    dataRef.addValueEventListener(getData)
+//                    dataRef.addListenerForSingleValueEvent(getData)
                 }
             }
         }
@@ -48,5 +59,7 @@ class LoginActivity : AppCompatActivity() {
                 allButtons(i)
             }
         }
+
     }
 }
+
--- app/src/main/java/com/example/fitnessa/MainActivity.kt
+++ app/src/main/java/com/example/fitnessa/MainActivity.kt
@@ -2,30 +2,28 @@ package com.example.fitnessa
 
 import android.app.DatePickerDialog
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.view.Window
 import android.view.WindowManager
-import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.database.FirebaseDatabase
 import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
-import java.util.stream.IntStream.range
-import android.R.id
-
-
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         requestWindowFeature(Window.FEATURE_NO_TITLE)
-        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        this.window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN)
         setContentView(R.layout.activity_main)
         val calDate = Calendar.getInstance()
         val calYear = calDate.get(Calendar.YEAR)
         val calMonth = calDate.get(Calendar.MONTH)
         val calDay = calDate.get(Calendar.DAY_OF_MONTH)
+        val dataRef = FirebaseDatabase.getInstance().reference
+        var currentDate = "$calYear--$calMonth--$calDay"
         fun allButtons(id: View){
             when(id){
                 bmiBtn -> {
@@ -37,9 +35,9 @@ class MainActivity : AppCompatActivity() {
                     startActivity(intent)
                 }
                 calendarBtn -> {
-                    val calendar = DatePickerDialog(this, {view, year, month, dayOfMonth ->
-//                        var currentDate = year.toString() + "--" + (month + 1).toString() + "--" + dayOfMonth.toString()
-//                        intent.putExtra("currentDate", currentDate)
+                    val calendar = DatePickerDialog(this, { _, year, month, dayOfMonth ->
+                        currentDate = ""
+                        currentDate += year.toString() + "--" + (month + 1).toString() + "--" + dayOfMonth.toString()
                     }, calYear, calMonth, calDay)
                     calendar.show()
                 }
@@ -48,36 +46,57 @@ class MainActivity : AppCompatActivity() {
                     var y = 0
                     var z = 0
                     if (rep1.text.toString() != "" && set1.text.toString() != "" && lbs1.text.toString() != ""){
-                        var volume1 = rep1.text.toString().toInt() * set1.text.toString().toInt() * lbs1.text.toString().toInt()
-                        vol1.setText(volume1.toString())
+                        val volume1 = rep1.text.toString().toInt() * set1.text.toString().toInt() * lbs1.text.toString().toInt()
+                        vol1.text = volume1.toString()
                         x += volume1
                     }
                     if (rep2.text.toString() != "" && set2.text.toString() != "" && lbs2.text.toString() != "") {
-                        var volume2 = rep2.text.toString().toInt() * set2.text.toString().toInt() * lbs2.text.toString().toInt()
-                        vol2.setText(volume2.toString())
+                        val volume2 = rep2.text.toString().toInt() * set2.text.toString().toInt() * lbs2.text.toString().toInt()
+                        vol2.text = volume2.toString()
                         y += volume2
                     }
                     if (rep3.text.toString() != "" && set3.text.toString() != "" && lbs3.text.toString() != "") {
-                        var volume3 = rep3.text.toString().toInt() * set3.text.toString().toInt() * lbs3.text.toString().toInt()
-                        vol3.setText(volume3.toString())
+                        val volume3 = rep3.text.toString().toInt() * set3.text.toString().toInt() * lbs3.text.toString().toInt()
+                        vol3.text = volume3.toString()
                         z += volume3
                     }
-                    var totalVol = x + y + z
-                    volume.setText(totalVol.toString())
+                    val totalVol = x + y + z
+                    volume.text = totalVol.toString()
                 }
                 resetBtn2 -> {
-                    var allFields = arrayOf(rep1, rep2, rep3, set1, set2, set3, lbs1, lbs2, lbs3, vol1, vol2, vol3, volume)
+                    val allFields = arrayOf(rep1, rep2, rep3, set1, set2, set3, lbs1, lbs2, lbs3, vol1, vol2, vol3, volume)
                     for (i in allFields){
-                        i.setText("")
+                        i.text = ""
+                    }
+                }
+                addDataBtn -> {
+                    val serialId = intent.getStringExtra("serialId").toString()
+                    dataRef.child(serialId).child(currentDate).child("Total Volume").setValue(volume.text.toString())
+                    if (rep1.text.toString() != "" && set1.text.toString() != "" && lbs1.text.toString() != ""){
+                        val workout1 = workout1.text.toString() + "(" + rep1.text.toString() + "repx" + set1.text.toString() + "setx" + lbs1.text.toString() + "lbs=" + vol1.text.toString() + ")"
+                        dataRef.child(serialId).child(currentDate).child("Workout 1").setValue(workout1)
+                    }
+                    if (rep2.text.toString() != "" && set2.text.toString() != "" && lbs2.text.toString() != "") {
+                        val workout2 = workout2.text.toString() + "(" + rep2.text.toString() + "repx" + set2.text.toString() + "setx" + lbs2.text.toString() + "lbs=" + vol2.text.toString() + ")"
+                        dataRef.child(serialId).child(currentDate).child("Workout 2").setValue(workout2)
+                    }
+                    if (rep3.text.toString() != "" && set3.text.toString() != "" && lbs3.text.toString() != "") {
+                        val workout3 = workout3.text.toString() + "(" + rep3.text.toString() + "repx" + set3.text.toString() + "setx" + lbs3.text.toString() + "lbs=" + vol3.text.toString() + ")"
+                        dataRef.child(serialId).child(currentDate).child("Workout 3").setValue(workout3)
                     }
                 }
             }
         }
-        val buttons = arrayOf(bmiBtn, loginBtn, calendarBtn, calcBtn, resetBtn2)
+        val buttons = arrayOf(bmiBtn, loginBtn, calendarBtn, calcBtn, resetBtn2, addDataBtn)
         for (i in buttons){
             i.setOnClickListener {
                 allButtons(i)
             }
         }
+        var loginID = intent.getStringExtra("loginID").toString()
+        if (loginID == "null") {
+            loginID = "LOGIN"
+        }
+        loginBtn.text = loginID
     }
 }
